5febd1a1983fc409e291be2e1b728a6c2e2e4827
Zokir Sobirjonov <zokir5857@gmail.com>
2021-01-17 00:40:09 +0500

    CRUD Operations

---
+++ vega/Controllers/VehiclesController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vega.Controllers.Resources;
+using vega.Models;
+using vega.Persistance;
+
+namespace vega.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehiclesController: Controller
+    {
+        private readonly IMapper mapper;
+        private readonly VegaDbContext context;
+
+        public VehiclesController(IMapper mapper, VegaDbContext context)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateVehicle([FromBody]VehicleResource vehicleFeature)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleFeature);
+            vehicle.LastUpdate = DateTime.Now;
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody]VehicleResource vehicleFeature)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+                mapper.Map<VehicleResource, Vehicle>(vehicleFeature, vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var result = await context.Vehicles.FindAsync(id);
+
+            if (result == null)
+                return NotFound();
+            context.Remove(result);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var result = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (result == null)
+                return NotFound();
+
+            var vehicleResource = mapper.Map<Vehicle, VehicleResource>(result);
+            return Ok(vehicleResource);
+
+        }
+    }
+}
--- vega/Persistance/VegaDbContext.cs
+++ vega/Persistance/VegaDbContext.cs
@@ -4,12 +4,18 @@ namespace vega.Persistance
 {
     public class VegaDbContext: DbContext
     {
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Feature> Features { get; set; }
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
         :base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf =>
+            new { vf.FeatureId, vf.VehicleId });
+        }
 
-        public DbSet<Make> Makes { get; set; }
-        public DbSet<Feature> Features { get; set; }
     }
 }
